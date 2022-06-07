723f8fb09b219fbfe24f0472ef31f4e9bcfffccf
Daniela Esparza Espinosa <desparza13@fb.com>
2022-06-07 10:27:05 -0700

    Fix getHomeTimeline() by obtaining enhanced permission from the Twitter Developers portal, create two new models User and Tweet, that allows to obtain the data out of the JSON and create objects accordingly

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -26,12 +26,13 @@ public class TimelineActivity extends AppCompatActivity {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.i(TAG, "onSuccess");
+                Log.i(TAG, "onSuccess"+json.toString());
             }
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.i(TAG, "onFailure", throwable);
+                Log.i(TAG, response);
+                Log.i(TAG, "onFailure"+response, throwable);
 
             }
         });
